86eaf1c25e3a946654e18b30a68f52b48a734df9
Tapas Dalai <tapas.capiq@gmail.com>
2024-04-06 12:24:50 +0000

    dept_sal added

---
+++ models/dept_sal.sql
@@ -0,0 +1,4 @@
+with dept_sal as
+(select department_name,sum(salary) from dbt_db.dbt_prod_sch.employees e join dbt_db.dbt_prod_sch.departments d on e.department_id=d.department_id
+group by department_name)
+select * from dept_sal
